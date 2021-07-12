6cefccffcbd030f5e3ddbdba4821f8778eb6892e
Yefei <316606233@qq.com>
2021-07-12 17:18:18 +0800

    fix: mysql2 invalid options warning

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const mysql = require('mysql2');
 const { PoolQuery } = require('mysql-easy-query');
 
 /**
+ * @param {Object} obj 
+ * @param {string[]} keys
+ * @returns {Object}
+ */
+function popProperties(obj, keys) {
+  const pop = {};
+  keys.forEach(key => {
+    if (key in obj) {
+      pop[key] = obj[key];
+      delete obj[key];
+    }
+  });
+  return pop;
+}
+
+/**
  * @param {import('@zenweb/core').Core} core 
  * @param {object} [options]
  */
@@ -21,10 +37,13 @@ function setup(core, options) {
     contextProperty: 'db',
   }, options);
   debug('options: %o', options);
+  const otherOptions = popProperties(options, [
+    'contextProperty',
+  ]);
   const pool = mysql.createPool(options);
   const query = new PoolQuery(pool);
   Object.defineProperty(core, 'mysql', { value: query });
-  Object.defineProperty(core.koa.context, options.contextProperty, { value: query });
+  Object.defineProperty(core.koa.context, otherOptions.contextProperty, { value: query });
 }
 
 module.exports = {
